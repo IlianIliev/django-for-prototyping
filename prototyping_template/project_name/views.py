32c843bdf103026c8adf719da98f73fbeb777c4f
eng. Ilian Iliev <ilian@i-n-i.org>
2013-04-15 22:16:37 +0300

    View changes for 404 pages

--- prototyping_template/project_name/views.py
+++ prototyping_template/project_name/views.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.http import Http404
+from django.template import TemplateDoesNotExist
 from django.views.generic import TemplateView
 
 
@@ -6,4 +9,13 @@ class StaticPageView(TemplateView):
     def get(self, request, slug=None): 
         if slug:
             self.template_name = '{{ project_name }}/%s.html' % slug
-        return super(StaticPageView, self).get(request, slug)
+            request.page = slug
+        else:
+            request.page = 'home'
+        result = super(StaticPageView, self).get(request, slug)
+        if not settings.DEBUG:
+            try:
+                result.resolve_template(self.template_name)
+            except TemplateDoesNotExist, e:
+                raise Http404
+        return result
